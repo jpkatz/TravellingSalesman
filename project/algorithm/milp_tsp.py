446a82c09c5d808b89ebc88ed17ab040327525e8
jpkatz <jpkatz@DESKTOP-LC3DOSP>
2023-05-22 19:07:38 -0500

    Making tsp solver a class

--- project/algorithm/milp_tsp.py
+++ project/algorithm/milp_tsp.py
@@ -58,69 +58,76 @@ class TspConsHandler(popt.Conshdlr):
         for x_var in x.values():
             self.model.addVarLocks(x_var, nlocksneg, nlockspos)
 
-def solve_mip(model):
-    model.optimize()
-    for v in model.getVars():
-        if model.getVal(v) > 0.5:
-            print("%s: %d" % (v, round(model.getVal(v))))
+class TSP:
+
+    def __init__(self, graph) -> None:
+        self.graph = graph
+        self.model = self.generate_model()
+        self.x_ij = {}
 
 
-def build_mip(graph):
-    model = generate_model()
-    x_ij = build_variables(model, graph)
-    build_constraints(model, graph, x_ij)
-    build_objective(model, graph, x_ij)
-
-    constraint_hanlder = TspConsHandler(x_ij)
-    constraint_hanlder.add_node_names(graph)
-    model.includeConshdlr(constraint_hanlder, 'TSP', 'TSP Subtour Elimination', needscons=False)
-
-    return model
+    def solve_mip(self):
+        self.model.optimize()
+        for v in self.model.getVars():
+            if self.model.getVal(v) > 0.5:
+                print("%s: %d" % (v, round(self.model.getVal(v))))
 
 
-def generate_model():
-    model = popt.Model()
-    return model
+    def build_mip(self):
+        self.build_variables()
+        self.build_constraints()
+        self.build_objective()
+
+        constraint_hanlder = TspConsHandler(self.x_ij)
+        constraint_hanlder.add_node_names(self.graph)
+        self.model.includeConshdlr(constraint_hanlder, 'TSP', 'TSP Subtour Elimination', needscons=False)
 
 
-def build_variables(model, graph):
-    x_ij = {}
-    for first_city, connections in graph.items():
-        for second_city, distance in connections.items():
-            x_ij[first_city, second_city] = model.addVar(vtype='B', name="x_(%s,%s)" % (first_city, second_city))
-    return x_ij
+
+    def generate_model(self):
+        model = popt.Model()
+        return model
 
 
-def build_constraints(model, graph, x_ij):
-    # one exit from city
-    for city_i, connections in graph.items():
-        terms = []
-        for city_j in connections.keys():
-            term = x_ij[city_i, city_j]
-            terms.append(term)
-        model.addCons(popt.quicksum(terms) == 1)
-
-    # one entrance to city
-    for city_i, connections in graph.items():
-        terms = []
-        for city_j in connections.keys():
-            term = x_ij[city_j, city_i]
-            terms.append(term)
-        model.addCons(popt.quicksum(terms) == 1)
+    def build_variables(self):
+        for first_city, connections in self.graph.items():
+            for second_city, distance in connections.items():
+                self.x_ij[first_city, second_city] = self.model.addVar(vtype='B', 
+                                                                       name="x_(%s,%s)" 
+                                                                       % (first_city, second_city))
 
 
-def build_objective(model, graph, x_ij):
-    obj_terms = []
-    for first_city, connections in graph.items():
-        for second_city, distance in connections.items():
-            term = distance * x_ij[first_city, second_city]
-            obj_terms.append(term)
-    model.setObjective(popt.quicksum(obj_terms), "minimize")
+    def build_constraints(self):
+        # one exit from city
+        for city_i, connections in self.graph.items():
+            terms = []
+            for city_j in connections.keys():
+                term = self.x_ij[city_i, city_j]
+                terms.append(term)
+            self.model.addCons(popt.quicksum(terms) == 1)
+
+        # one entrance to city
+        for city_i, connections in self.graph.items():
+            terms = []
+            for city_j in connections.keys():
+                term = self.x_ij[city_j, city_i]
+                terms.append(term)
+            self.model.addCons(popt.quicksum(terms) == 1)
+
+
+    def build_objective(self):
+        obj_terms = []
+        for first_city, connections in self.graph.items():
+            for second_city, distance in connections.items():
+                term = distance * self.x_ij[first_city, second_city]
+                obj_terms.append(term)
+        self.model.setObjective(popt.quicksum(obj_terms), "minimize")
 
 
 if __name__ == '__main__':
-    path_to_test = '../test_bank/4_city_test.json'
+    path_to_test = './project/test_bank/4_city_test.json'
     f = open(path_to_test)
     test_problem = json.load(f)
-    model = build_mip(test_problem)
-    solve_mip(model)
+    tsp = TSP(test_problem)
+    tsp.build_mip()
+    tsp.solve_mip()
